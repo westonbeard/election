31a1b6b0ac9cc07725534bd6e738112f4946d4fe
Weston Beard <weston.beard@gmail.com>
2015-09-21 18:27:32 -0400

    annotating models, tests, fixtures, factories

--- app/models/voter.rb
+++ app/models/voter.rb
@@ -1,2 +1,17 @@
+# == Schema Information
+#
+# Table name: voters
+#
+#  id         :integer          not null, primary key
+#  first_name :string
+#  last_name  :string
+#  address    :string
+#  city       :string
+#  state      :string
+#  zip        :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Voter < ActiveRecord::Base
 end
--- test/models/voter_test.rb
+++ test/models/voter_test.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: voters
+#
+#  id         :integer          not null, primary key
+#  first_name :string
+#  last_name  :string
+#  address    :string
+#  city       :string
+#  state      :string
+#  zip        :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class VoterTest < ActiveSupport::TestCase
